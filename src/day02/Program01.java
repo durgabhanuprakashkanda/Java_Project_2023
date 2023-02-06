12954089ab37e7a9c74872ff31c731f4f13dd42e
KANDA DURGA BHANU PRAKASH <BHANUPRAKASHKANDA123@GMAIL.COM>
2023-02-06 09:01:56 +0530

    Day02
    Program01 and Program02

--- src/day02/Program01.java
+++ src/day02/Program01.java
@@ -2,9 +2,29 @@ package day02;
 
 public class Program01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void fun()
+	{
+		int x=101234567;
+		char y='a';
+		boolean a=true;
+		byte b=127;
+		short c=32000;
+		long d=1234567890l;
+		double a1;
+		a1=12345.4567d;
+		float a2=23456.67f;
+		System.out.println(a2);
+		System.out.println(a1);
+		System.out.println(d);
+		System.out.println("Good Morning..!");
 	}
 
+	
+	public static void main(String[] args) {
+		System.out.println("Good");
+		System.out.println(" day");
+		fun();
+	}
+	
 }
