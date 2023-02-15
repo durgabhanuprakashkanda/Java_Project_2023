48fed57f2be871f9fbc1297d1bb183ff819c7da8
KANDA DURGA BHANU PRAKASH <BHANUPRAKASHKANDA123@GMAIL.COM>
2023-02-15 22:29:17 +0530

    Day07

--- src/day07/Program.java
+++ src/day07/Program.java
@@ -1,9 +1,44 @@
 package day07;
-
+import java.util.Scanner;
 public class Program {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner x= new Scanner(System.in);
+		System.out.println("Enter the Numbers:-  ");
+		int a=x.nextInt();
+        int b=x.nextInt();
+        int c=x.nextInt();
+		x.close();
+
+
+		if(a>b && a>c)
+		{  
+			System.out.println("a is greater..!");  
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println("b is greater..!");   
+		}
+		else if(c>a && c>b)
+		{
+			System.out.println("c is greater..!");   
+		}
+		else if(a==b && a==c)
+		{
+			System.out.println("a & b & c are equal..!");
+		}
+		else if(a==c)
+		{
+			System.out.println("a & c are equal..!");
+		}
+		else if(c==b)
+		{
+			System.out.println("c & b are equal..!");
+		}
+		else 
+		{
+			System.out.println("a & b are equal..!");
+		}
+		
 
 	}
 
