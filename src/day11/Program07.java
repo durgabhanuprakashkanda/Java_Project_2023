6da35c03be1199049d817b53d8b4348feac7f64f
KANDA DURGA BHANU PRAKASH <BHANUPRAKASHKANDA123@GMAIL.COM>
2023-02-24 18:11:35 +0530

    Day11
    work

--- src/day11/Program07.java
+++ src/day11/Program07.java
@@ -15,6 +15,13 @@ public class Program07 {
 			System.out.println();
 		}
 
+		
+		for(int i=5;i>=1;i--) {
+			for(int j=5;j>=i;j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
 	}
 
 }
--- src/day11/Program08.java
+++ src/day11/Program08.java
@@ -7,9 +7,26 @@ public class Program08 {
 		2 4
 		3 9 3
 		4 16 4 16
-		5 25 5 25 5*/
+		5 25 5 25 5
+		
+		5 25 5 25 5 
+		4 16 4 16 
+		3 9 3 
+		2 4 
+		1 */
 		
 		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				if(j%2==0) {
+					System.out.print(i*i+" ");
+				}
+				else System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+		
+		
+		for(int i=5;i>=1;i--) {
 			for(int j=1;j<=i;j++) {
 				if(j%2==0) {
 					System.out.print(i*i+" ");
