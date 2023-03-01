a086c6fa09e653e444ec2ab2d583734467f21557
KANDA DURGA BHANU PRAKASH <BHANUPRAKASHKANDA123@GMAIL.COM>
2023-03-01 15:26:15 +0530

    Day14

---
+++ src/day14/Program05.java
@@ -0,0 +1,38 @@
+package day14;
+
+public class Program05 {
+
+	public static void main(String[] args) {
+		// Hex to decimal
+		/*Scanner x=new Scanner(System.in);
+		System.out.println("Enter the Hex number");
+		 String[] a = new String[7];
+		x.close();
+		String[] ch = new String[a.length()];
+		String[] b = new String[a.length()];
+		  
+       
+        for (int i = 0; i < a.length(); i++) {
+            ch[i] = a[i];
+            int y=ch[i];
+            if(y>64&&y<71) {
+            	if(y==65)
+            	 b[i]="10";
+            	else if(y==66)
+            		b[i]="11";
+            	else if(y==67)
+            		b[i]="12";
+            	else if(y==68)
+            		b[i]="13";
+            	else if(y==69)
+            		b[i]="14";
+            	else b[i]="15";
+            }
+            else b[i]=ch[i];
+        }
+		System.out.println(ch);*/
+		
+
+	}
+
+}
