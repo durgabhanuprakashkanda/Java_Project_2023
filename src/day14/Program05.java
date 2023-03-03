c8b4cdce6a32d7b449812c8042f1cc61746cd554
KANDA DURGA BHANU PRAKASH <BHANUPRAKASHKANDA123@GMAIL.COM>
2023-03-03 09:04:46 +0530

    Day16

--- src/day14/Program05.java
+++ src/day14/Program05.java
@@ -1,36 +1,14 @@
 package day14;
 
+import java.util.ArrayList;
+
 public class Program05 {
 
 	public static void main(String[] args) {
 		// Hex to decimal
-		/*Scanner x=new Scanner(System.in);
+		ArrayList<String> x= new ArrayList<>();
 		System.out.println("Enter the Hex number");
-		 String[] a = new String[7];
-		x.close();
-		String[] ch = new String[a.length()];
-		String[] b = new String[a.length()];
-		  
-       
-        for (int i = 0; i < a.length(); i++) {
-            ch[i] = a[i];
-            int y=ch[i];
-            if(y>64&&y<71) {
-            	if(y==65)
-            	 b[i]="10";
-            	else if(y==66)
-            		b[i]="11";
-            	else if(y==67)
-            		b[i]="12";
-            	else if(y==68)
-            		b[i]="13";
-            	else if(y==69)
-            		b[i]="14";
-            	else b[i]="15";
-            }
-            else b[i]=ch[i];
-        }
-		System.out.println(ch);*/
+		
 		
 
 	}
